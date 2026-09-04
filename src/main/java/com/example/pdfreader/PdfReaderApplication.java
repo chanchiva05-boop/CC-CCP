170fc5e7d2fcfab5c6afe7891f84b3177b592342
Chan Chiva <chanchiva05@gmail.com>
2026-09-04 22:04:10 +0700

    Update PdfReaderApplication.java

--- src/main/java/com/example/pdfreader/PdfReaderApplication.java
+++ src/main/java/com/example/pdfreader/PdfReaderApplication.java
@@ -1,1 +1,13 @@
-hrkk
+package com.example.pdfreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfReaderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PdfReaderApplication.class, args);
+        System.out.println("✅ Server started at: http://localhost:8080");
+        System.out.println("📁 Put PDF files in: ./src/main/webapp/pdfs/");
+    }
+}
